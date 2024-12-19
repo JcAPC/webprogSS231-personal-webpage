d950c4846236f6d28ba6fc15f27ec79462ec9180
JcAPC <jaycemarkpresbitero1902@gmail.com>
2024-12-19 17:48:31 +0800

    Updated

--- js/myScript.js
+++ js/myScript.js
@@ -1,8 +1,13 @@
+// Function to update the paragraph to "A paragraph"
+function myFunction() {
+    document.getElementById("demo").innerHTML = "A paragraph.";
+}
+
+// Function to display the content of the first paragraph
 function myFunction2() {
     const element = document.getElementsByTagName("p");
-
     const firstParagraphText = element[0].innerHTML;
 
     document.getElementById("demo").innerHTML =
-        "<br> The text in the first paragraph (index 0) is: <br>" + firstParagraphText;
+        "The text in the first paragraph (index 0) is: <br>" + firstParagraphText;
 }
